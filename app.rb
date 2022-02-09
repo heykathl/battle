748552e1ea3da07d9ef24024cb4cbd480d5834ad
Kathleen Ly <lykathleen@outlook.com>
2022-02-09 14:49:52 +0000

    20. POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -6,20 +6,26 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
+enable :sessions
+
 get '/' do
-erb :index
+  erb :index
 end
 
 post '/names' do
   p params
   # erb :index
-  @player_1 = params[:Player_1]
-  @player_2 = params[:Player_2]
-  erb :play
+  session[:Player_1] = params[:Player_1]
+  session[:Player_2] = params[:Player_2]
+  redirect '/play'
   # "#{@player_1} #{@player_2}"
 end
 
-
+get '/play' do
+  @player_1 = session[:Player_1]
+  @player_2 = session[:Player_2]
+  erb :play
+end
 
   # to start server when ruby app.rb is called
   run! if app_file == $0
