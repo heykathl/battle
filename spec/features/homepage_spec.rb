d7715fa3c90737de283934c27ad77c2fcf9e605b
Jack <winterschladen.j@gmail.com>
2022-02-09 15:09:44 +0000

    21 & 22. Viewing hit-points & refactored tests with test helpers

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -6,17 +6,21 @@ feature "Home page" do
     visit('/')
     expect(page).to have_content('Welcome to Battle!')
   end
+end
 
+feature "Player names" do
   scenario "saves and diplays names that are entered" do
-    visit('/')
-    fill_in('Player_1', with: 'Jack')
-    fill_in('Player_2', with: 'Kathleen')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('Player 1: Jack')
     expect(page).to have_content('Player 2: Kathleen')
 
     # save_and_open_page
-    
   end
 end
 
+feature "Display hit points" do
+  scenario "shows player 2's hit points" do
+    sign_in_and_play
+    expect(page).to have_content('Kathleen hit points: 100')
+  end
+end
