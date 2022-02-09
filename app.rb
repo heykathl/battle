bce0295e11547ef0358583529f6c9e13ede5de83
Jack <winterschladen.j@gmail.com>
2022-02-09 14:02:44 +0000

    19. Entering Players

--- app.rb
+++ app.rb
@@ -7,8 +7,20 @@ class Battle < Sinatra::Base
   end
 
 get '/' do
-  "Testing infrastructure working!"
+  # "Testing infrastructure working!"
+  erb :index
 end
+
+post '/names' do
+  p params
+  # erb :index
+  @player_1 = params[:Player_1]
+  @player_2 = params[:Player_2]
+  erb :play
+  # "#{@player_1} #{@player_2}"
+end
+
+
 
   # to start server when ruby app.rb is called
   run! if app_file == $0
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -2,8 +2,18 @@ require_relative '../../app.rb'
 # require 'capybara/rspec'
 
 feature "Home page" do
-  scenario "should return a message" do
+  # scenario "should return a message" do
+  #   visit('/')
+  #   expect(page).to have_content('Testing infrastructure working!')
+  # end
+
+  scenario "saves and diplays names that are entered" do
     visit('/')
-    expect(page).to have_content('Testing infrastructure working!')
+    fill_in('Player_1', with: 'Jack')
+    fill_in('Player_2', with: 'Kathleen')
+    click_button('Submit')
+    expect(page).to have_content('Player 1: Jack')
+    expect(page).to have_content('Player 2: Kathleen')
   end
 end
+
